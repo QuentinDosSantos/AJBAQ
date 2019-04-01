dc631afd858c41a01d8374e3d8ed906ddaa44d46
Quentin <shimotakanaki@gmail.com>
2019-04-01 15:01:39 +0200

    MAJ connectionstring

--- AJBAQ.DAL/AJBAQContext.cs
+++ AJBAQ.DAL/AJBAQContext.cs
@@ -26,7 +26,7 @@ namespace AJBAQ.DAL
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-MC110OI\\QDSERVER;Database=AJBAQ;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=tcp:pownstars.database.windows.net,1433;Initial Catalog=AJBAQ;Persist Security Info=False;User ID=pownstars;Password=?SEoKS^tB?pIt9pr;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 
